07513b80248e00c4f8415abf61b995c967563813
Colin Chartier <colin.n.chartier@gmail.com>
2015-11-18 21:13:23 -0500

    Fix up antlr being called in the gradle build, change package.

---
+++ src/main/java/com/colinchartier/probo/Test.java
@@ -0,0 +1,9 @@
+package com.colinchartier.probo;
+
+
+public class Test {
+    public static void main(String[] args) {
+       com.colinchartier.probo.gen.ProofParser p = new com.colinchartier.probo.gen.ProofParser(null);
+        System.out.println(p.getClass());
+    }
+}
